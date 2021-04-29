a4c46cdec80877312afd13de8937d21fdfe9bd1b
Alexandre <50714185+Alekzus@users.noreply.github.com>
2021-04-29 12:03:30 +0200

    Removed CLIENT_ID

--- app/src/main/java/com/alexandre/marcq/spotiguessr/utils/Constants.kt
+++ app/src/main/java/com/alexandre/marcq/spotiguessr/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.alexandre.marcq.spotiguessr.utils
 import com.alexandre.marcq.spotiguessr.R
 
 object Constants {
-    const val CLIENT_ID = "54e6af2cfb524c1ab6de551d677c6131"
+    const val CLIENT_ID = "CLIENT_ID"
     const val REQUEST_CODE = 1234
     const val REDIRECT_URI = "com.alexandre.marcq.spotiguessr://callback"
     const val BASE_URL = "https://api.spotify.com/v1/"
